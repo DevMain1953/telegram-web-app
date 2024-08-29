e738a27fc9009d524d97dced7bcc0ca5d74ddb5a
sergey <sergey@sergey.sergey>
2024-08-29 12:01:44 +0100

    changed simple url opening in browser with real telegram web app

--- bot.py
+++ bot.py
@@ -1,29 +1,38 @@
 import os
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import (
+    Message,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    WebAppInfo,
+)
 from aiogram.filters import Command
 from dotenv import load_dotenv
 
 
 load_dotenv()
-API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+TELEGRAM_API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 
 
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=TELEGRAM_API_TOKEN)
 dp = Dispatcher()
 
 
 @dp.message(Command("start"))
-async def send_button(message: Message) -> None:
+async def send_message_with_button_to_client(message: Message) -> None:
     """
-    Sends a message with button to client.
+    Sends a message with button to client that opens Telegram Web App.
 
     :param message: A message that is sent to bot from client.
     :type message: Message
     """
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="Заполнить", url="https://url-to-ngrok")]
+            [
+                InlineKeyboardButton(
+                    text="Заполнить", web_app=WebAppInfo(url="https://to-do")
+                )
+            ]
         ]
     )
     await message.answer(
