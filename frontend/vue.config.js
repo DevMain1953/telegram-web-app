aff9de285623296b2cfb692c5e15f8106f831a1d
sergey <sergey@sergey.sergey>
2024-08-29 15:31:00 +0100

    fixed websockets

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    https: true,
+    client: {
+      webSocketURL: {
+        protocol: 'wss',
+        hostname: 'url-to-ngrok',
+        port: 443
+      }
+    },
+  }
+});
